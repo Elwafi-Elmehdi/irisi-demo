c0c3b902c4cd71fc6d8b091bbb80d5ad3fa7a435
Mehdi El Wafi <mehdi.elwafi@youcan.shop>
2024-10-24 12:01:28 +0100

    Initial config

---
+++ app/src/test/java/ma/irisi/irisi/IrisiApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.irisi.irisi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
