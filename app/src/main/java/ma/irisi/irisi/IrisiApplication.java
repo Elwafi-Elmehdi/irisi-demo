2c3bd15ad08e0c03aacc1b483f8dc74c9e0ca9c0
Mehdi El Wafi <mehdi.elwafi@youcan.shop>
2024-10-24 12:01:28 +0100

    Initial config

---
+++ app/src/main/java/ma/irisi/irisi/IrisiApplication.java
@@ -0,0 +1,13 @@
+package ma.irisi.irisi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrisiApplication.class, args);
+	}
+
+}
